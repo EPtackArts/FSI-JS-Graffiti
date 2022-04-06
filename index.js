43108d3f42a553066a916698109570b9c4133acf
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-04-05 19:30:03 -0500

    first save

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 
 // Select some elements...
+let header = document.querySelector('#pager-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll(".")
+console.log(header)
